33d77624edde1f828bf5f3c3a1a5af8e26ac7dcb
Maria Loza <orcaluver@yahoo.com>
2017-03-13 20:50:55 +0000

    updated cairo

--- cairo_proj.py
+++ cairo_proj.py
@@ -9,23 +9,26 @@ from random import random
 import cairo
 import shutil
 
-myList = []
+#UPLOADED IMAGE SHOULD BE SQUARE FOR CORRECT SCALING
+
+myList = [] #stores image
 # print("Input complete image filename: ")
 # image = raw_input();
 # myList.append(Image.open(image))
 # width, height  = myList[0].size
 PI = 3.14159265359
 TWOPI = 2.0*PI
-BACK = [0,0,0,1]
-FRONT = [0,0,0,1.0]
+BACK = [0,0,0,1] #background image
+FRONT = [0,0,0,1.0] #default color of circle
 SIZE = 1000
 ONE = 1/SIZE
 LINEWIDTH = ONE
 
-STEPS = 1000
+STEPS = 1000 #steps controls potential max size of circles
 
-NEW_COUNT = 100
+NEW_COUNT = 100 #new_count controls number of new cicles added per instance
 
+RADIUS = 5; #radius sets the default smallest circle radius, smaller numbers will generate images with tiny circles
 
 # #STEPS = 250
 # #NEW_COUNT = 600
@@ -36,7 +39,7 @@ NEW_COUNT = 100
 # NEW_COUNT = input();
 
 
-RES = 'out.png'
+RES = 'out.png' #name of output image
 
 def getNewFile():
   return RES
@@ -45,26 +48,29 @@ def imageSrc(msg):
     myList.append(Image.open(msg))
     global width
     global height
-    width, height  = myList[0].size
+    width, height  = myList[0].size #size of image
 
-def is_ok(x1, y1, r1, x2, y2, r2):
+def is_ok(x1, y1, r1, x2, y2, r2):  #checks if a new cicle to be added will not overlap with any other circles
   return ((x1-x2)**2 + (y1-y2)**2) > (r1 + r2 + ONE)**2
+  #returns true if difference between x and y values of cicles (squared to make positive)
+  #are greater than the difference between the radius of the cicles(squared to make positive)
 
-def add_new_circles(n, circles):
+def add_new_circles(n, circles):  #method to add new circles, n is new_count to be added
 
-  for i in xrange(n):
+  for i in xrange(n): #picks random x and y
     xn = random()
     yn = random()
     rn = ONE
+    # rn = RADIUS
 
     ok = True
     for c in circles:
 
-      if not is_ok(xn, yn, rn, c['x'], c['y'], c['r']):
+      if not is_ok(xn, yn, rn, c['x'], c['y'], c['r']): #checks for overlap
         ok = False
         break
 
-    if ok:
+    if ok: #add new circle to circles array
       circles.append({
         'x': xn, 
         'y': yn, 
@@ -72,13 +78,13 @@ def add_new_circles(n, circles):
         'active': True
       })
 
-def increase_radius(circles):
+def increase_radius(circles): #as program runs, increase the potential size of circles to be added
 
-  for c in circles:
+  for c in circles: 
     if c['active']:
-      c['r'] += ONE
+      c['r'] += ONE #adds .001 to radius size
 
-def test(circles):
+def test(circles): #no clue --------------------
 
   ok_count = 0
 
@@ -103,14 +109,14 @@ def test(circles):
 
 def show(ctx, circles):
 
-  ctx.set_source_rgba(*FRONT)
+  ctx.set_source_rgba(*FRONT) #default color of circle
 
   for c in circles:
-    r, g, b = myList[0].getpixel((c['x'] * width, c['y'] * height))
-    rgba = [r/256, g/256, b/256, 1.0]
-    ctx.set_source_rgba(*rgba)
-    ctx.arc(c['x'], c['y'], c['r'], 0, TWOPI)
-    ctx.fill()
+    r, g, b = myList[0].getpixel((c['x'] * width, c['y'] * height)) #grabs rgb value from image
+    rgba = [r/256, g/256, b/256, 1.0]  #formats rgb values
+    ctx.set_source_rgba(*rgba) #assigns rgb value
+    ctx.arc(c['x'], c['y'], c['r'], 0, TWOPI) #creates circle
+    ctx.fill() #fills with correct rgb value
 
 def main():
 
@@ -130,7 +136,7 @@ def main():
   ctx.set_line_width(LINEWIDTH)
 
 
-  circles = []
+  circles = [] #main circle arrray to store all circles locations and radius values
   add_new_circles(NEW_COUNT, circles)
 
   for i in xrange(STEPS):
