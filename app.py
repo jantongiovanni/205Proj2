548234f354afce3d048e535ea1b0c95945a7d607
Maria Loza <orcaluver@yahoo.com>
2017-03-14 17:34:38 +0000

    clearing cache works

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -32,14 +35,24 @@ def index():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             if request.form['steps'] and request.form['count'] and request.form['radius']:
+                newFile = cairo_proj.getNewFile()
+                # my_file = Path("static/circlePacked"+newFile)
+                if os.path.isfile("static/circlePacked/"+newFile):
+                    os.remove("static/circlePacked/"+newFile)
                 steps = request.form['steps']
+                # cairo_proj.setSteps(steps)
+                count = request.form['count']
+                # cairo_proj.setCount(count)
+                # radius = request.form['radius']
+                # cairo_proj.setRadius(radius)
+                print cairo_proj.NEW_COUNT
+                # print cairo_proj
                 # print steps
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 # # return redirect(url_for('uploaded_file', filename=filename))
                 cairo_proj.imageSrc(UPLOAD_FOLDER+'/'+filename)
                 cairo_proj.main()
-                newFile = cairo_proj.getNewFile()
                 # shutil.move("out.png", "static/circlePacked/"+newFile)
                 return flask.render_template("index.html", filename=filename, newFile=newFile)
             else:
